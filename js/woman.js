2419da58165eb00df9564985d80e1272f3eb1ade
rony3516 <121095564+rony3516@users.noreply.github.com>
2023-04-03 15:02:40 +0600

    Add files via upload

---
+++ js/woman.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#loginBtn').click(function(){
+    $('.login').slideToggle(1000)
+    });
+    });
+
+    /*wow js work*/
+    new WOW().init();
+    /*wow js end */
+
+$(document).ready(function(){
+$("#AllProduct").hide()
+$("#seeOur").click(function(){
+$("#AllProduct").slideToggle(1000)
+})
+ 
+
+})
+
+
